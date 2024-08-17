33825dbadb74b4b530783a3eb5aa78261172a05e
Minjeong Yuk <oasis@Minjeongs-Laptop.local>
2024-08-17 14:50:43 +0900

    features

--- components/feature_teamspace.tsx
+++ components/feature_teamspace.tsx
@@ -1,7 +1,21 @@
 export default function FeatureTeamSpace({ data }: { data: any }) {
     return (
-        <div>
-            <p>FeatureTeamSpace</p>
+        <div className="inline-block w-screen p-[80px] h-[800px] bg-blue-500 relative">
+            <div className="relative">
+                <div className="absolute top-0">
+                    <p className="text-xl font-bold">{data.title}</p>
+                    <p className="mt-5">{data.explanation1}</p>
+                    <p className="">{data.explanation2}</p>
+                    <p className="">{data.explanation3}</p>
+                </div>
+                <div className="absolute top-[500px]">
+                    <p className="text-2xl font-extrabold">{data.footer1}</p>
+                    <p className="text-2xl font-extrabold">{data.footer2}</p>
+                </div>
+                <div className="bg-gray-200 w-[550px] h-[400px] rounded ml-[100px] absolute right-0 top-[200px]">
+                    GIF
+                </div>
+            </div>
         </div>
     )
 }
