0c22118bb61e9d8c57f6fb8379548d3eed4b77b6
jaeyeonp <wodusqkr1210@gmail.com>
2024-08-17 15:38:52 +0900

    client_service waitlist added

--- components/wailitst.tsx
+++ components/wailitst.tsx
@@ -1,7 +1,15 @@
 export default function Waitlist({ data }: { data: any }) {
     return (
-        <div>
-            <p>Waitlist</p>
+        <div className="bg-gray-400 text-center inline-block w-full py-[160px]">
+            <div className="top">
+                <h1 className="text-5xl font-extrabold">{data.title1}</h1>
+                <h1 className="text-5xl font-extrabold">{data.title2}</h1>
+            </div>
+            <div className="bot">
+                <div className="w-[150px] h-[150px] bg-gray-300 border border-gray-400 flex items-center justify-center">
+                    크랭이 만나보기
+                </div>
+            </div>
         </div>
     )
 }
