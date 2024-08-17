f6fba1f6cd57aba8d9dfdb57ad2785e500c8afab
jaeyeonp <wodusqkr1210@gmail.com>
2024-08-17 17:23:33 +0900

    123

--- components/wailitst.tsx
+++ components/wailitst.tsx
@@ -1,12 +1,12 @@
 export default function Waitlist({ data }: { data: any }) {
     return (
-        <div className="bg-gray-400 text-center inline-block w-full py-[160px]">
+        <div className="bg-gray-400 text-center inline-block w-full py-[160px] flex flex-col items-center">
             <div className="top">
                 <h1 className="text-5xl font-extrabold">{data.title1}</h1>
                 <h1 className="text-5xl font-extrabold">{data.title2}</h1>
             </div>
             <div className="bot">
-                <div className="w-[150px] h-[150px] bg-gray-300 border border-gray-400 flex items-center justify-center">
+                <div className="w-[150px] h-[150px] bg-gray-300 border border-gray-400 flex items-center justify-center mt-20">
                     크랭이 만나보기
                 </div>
             </div>
