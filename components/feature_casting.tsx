5d1f89f3a3af7f2256416aa767baa027c301fab9
Minjeong Yuk <oasis@Minjeongs-Laptop.local>
2024-08-17 14:04:48 +0900

    casting

--- components/feature_casting.tsx
+++ components/feature_casting.tsx
@@ -1,7 +1,41 @@
 export default function FeatureCasting({ data }: { data: any }) {
     return (
         <div>
-            <p>FeatureCasting</p>
+            <div>
+                <p>
+                    {data.title}
+                </p>
+            </div>
+            <div>
+                <div></div>
+                <div>
+                    <p>
+                        {data.feature1_1}
+
+                    </p>
+                    <p>
+                        {data.feature1_2}
+                    </p>
+                    <p>
+                        {data.explanation1}
+                    </p>
+                </div>
+            </div>
+            <div>
+                <div></div>
+                <div>
+                    <p>
+                        {data.feature2_1}
+
+                    </p>
+                    <p>
+                        {data.feature2_2}
+                    </p>
+                    <p>
+                        {data.explanation2}
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
