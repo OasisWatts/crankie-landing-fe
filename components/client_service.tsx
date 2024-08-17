fe47a12c50bdbe71212ccf474444f6676850b615
jaeyeonp <wodusqkr1210@gmail.com>
2024-08-17 18:23:27 +0900

    flex added

--- components/client_service.tsx
+++ components/client_service.tsx
@@ -6,10 +6,10 @@ export default function ClientService({ data }: any) {
                 <h1 className="text-5xl font-extrabold">{data.title1_2}</h1>
                 <h1 className="text-5xl font-extrabold">{data.title1_3}</h1>
             </div>
-            <div className="bot">
+            <div className="bot mt-20 mb-10">
                 <h1 className="text-5xl font-extrabold">{data.title2}</h1>
             </div>
-            <div className="btn">
+            <div className="btn mt-40">
                 <button>{data.button}</button>
             </div>
         </div>
