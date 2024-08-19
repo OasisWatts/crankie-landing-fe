0eb4a3445b346ab5fea2f2c74855b1ef8d298d5e
HyunHo Lee <hyunho741@gmail.com>
2024-08-19 10:03:45 +0900

    init

--- components/problem.tsx
+++ components/problem.tsx
@@ -13,10 +13,10 @@ export default function Problem({ data }: { data: { title1: string, title2: stri
             <p className="mt-5">{data.paragraph1}</p>
             <p>{data.paragraph2}</p>
 
-            <div className="mt-[50px] w-[1070px] h-[600px] rounded-md bg-gray-300 flex-auto items-center justify-center">
-                <p className="text-center py-[290px]">image</p>
+            <div className="mt-[50px] w-[1070px] h-[600px] rounded-md bg-[#2A2829] flex flex-col items-center justify-center">
+                <div className="w-[900px] h-[150px] bg-white rounded-md"></div>
+                <div className="mt-[40px] w-[900px] h-[150px] bg-white rounded-md"></div>
             </div>
-
         </div>
     )
 }
