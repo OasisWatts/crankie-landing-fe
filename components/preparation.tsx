8f901805d1598639bd0a8e73ed1b652252e5755c
jaeyeonp <wodusqkr1210@gmail.com>
2024-08-21 10:32:08 +0900

    코드 정리

--- components/preparation.tsx
+++ components/preparation.tsx
@@ -3,15 +3,15 @@
 import { useState, useEffect } from "react";
 
 export default function Preparation({ data }: { data: any }) {
-    const [currentExp, setCurrentExp] = useState(1); // 현재 강조된 exp
-    const expCount = 3; // exp의 수
+    const [currentExp, setCurrentExp] = useState(1); 
+    const expCount = 3; 
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentExp((prevExp) => (prevExp % expCount) + 1); // 1 -> 2 -> 3 -> 1 ...
-        }, 5000);
+            setCurrentExp((prevExp) => (prevExp % expCount) + 1);
+        }, 3000);
 
-        return () => clearInterval(interval); // 컴포넌트 언마운트 시 타이머 정리
+        return () => clearInterval(interval);
     }, []);
 
     return (
@@ -27,88 +27,52 @@ export default function Preparation({ data }: { data: any }) {
                 </div>
             </div>
             <div className="content bg-slate-500 h-[70vh] flex flex-row">
-                <div className="left bg-slate-700 flex-1 flex items-center justify-center" style={{ flexBasis: '50%' }}>
-                    <div className="image w-[90px] h-[90px] bg-red-100 flex items-center justify-center">
+                <div className="left bg-slate-700 flex-1 flex items-center justify-end" style={{ flexBasis: '50%' }}>
+                    <div className="image w-[400px] h-[400px] bg-red-100 flex items-center justify-center mr-10">
                         image {currentExp}
                     </div>
                 </div>
+                {/*설명란*/}
                 <div className="right flex-1 flex flex-col" style={{ flexBasis: '50%' }}>
-                    <div className={`exp1 flex-1 ${currentExp === 1 ? "bg-blue-500" : "bg-blue-50"}`}>
-                        explanation1
+                    {/*설명1 ${currentExp === 1 ? "bg-blue-500" : "bg-blue-50"}*/}
+                    <div className={`exp1 flex flex-row items-center justify-center flex-1 `}>
+                        <div className={`left h-full flex-1 flex items-center justify-center ${currentExp === 1 ? "bg-blue-500" : "bg-blue-100"}`} style={{flexBasis: "20%"}}>
+                            <div className="w-[95px] h-[95px] bg-gray-300 border border-gray-400 flex items-center justify-center">
+                                 이미지
+                            </div>
+                        </div>
+                        <div className={`right h-full flex-1 flex flex-col items-start justify-center ${currentExp === 1 ? "bg-blue-500" : "bg-blue-50"}`} style={{flexBasis: "80%"}}>
+                            <h1 className="text-3xl font-extrabold">{data.explanation1}</h1>
+                            <h1 className="text-lg font-extrabold">{data.explanation1_1}</h1>
+                        </div>
                     </div>
-                    <div className={`exp2 flex-1 ${currentExp === 2 ? "bg-blue-500" : "bg-blue-200"}`}>
-                        explanation2
+                    {/*설명2*/}
+                    <div className={`exp2 flex flex-row items-center justify-center flex-1`}>
+                        <div className={`left h-full flex-1 flex items-center justify-center ${currentExp === 2 ? "bg-blue-500" : "bg-blue-100"}`} style={{flexBasis: "20%"}}>
+                            <div className="w-[95px] h-[95px] bg-gray-300 border border-gray-400 flex items-center justify-center">
+                                 이미지
+                            </div>
+                        </div>
+                        <div className={`right h-full flex-1 flex flex-col items-start justify-center ${currentExp === 2 ? "bg-blue-500" : "bg-blue-50"}`} style={{flexBasis: "80%"}}>
+                            <h1 className="text-3xl font-extrabold">{data.explanation2}</h1>
+                            <h1 className="text-lg font-extrabold">{data.explanation2_1}</h1>
+                        </div>
                     </div>
-                    <div className={`exp3 flex-1 ${currentExp === 3 ? "bg-blue-500" : "bg-brown-400"}`}>
-                        explanation3
+                    {/*설명3*/}
+                    <div className={`exp3 flex flex-row items-center justify-center flex-1`}>
+                        <div className={`left h-full flex-1 flex items-center justify-center ${currentExp === 3 ? "bg-blue-500" : "bg-blue-100"}`} style={{flexBasis: "20%"}}>
+                            <div className="w-[95px] h-[95px] bg-gray-300 border border-gray-400 flex items-center justify-center">
+                                 이미지
+                            </div>
+                        </div>
+                        <div className={`right h-full flex-1 flex flex-col items-start justify-center ${currentExp === 3 ? "bg-blue-500" : "bg-blue-50"}`} style={{flexBasis: "80%"}}>
+                            <h1 className="text-3xl font-extrabold">{data.explanation3}</h1>
+                            <h1 className="text-3xl font-extrabold">{data.explanation3_0}</h1>
+                            <h1 className="text-lg font-extrabold">{data.explanation3_1}</h1>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
 }
-
-
-
-
-// export default function Preparation({ data }: { data: any }) {
-//     return (
-//         <div className="bg-gray-500 text-center h-screen w-full py-[160px] flex flex-col">
-//             {/* 상단 */}
-//             <div className="title flex flex-col justify-center items-center mb-40">
-//                 <div className="w-[90px] h-[90px] bg-gray-300 border border-gray-400 flex items-center justify-center">
-//                     이미지
-//                 </div>
-//                 <h1 className="text-3xl font-extrabold mt-10">{data.title}</h1>
-//             </div>
-//             {/* 하단 */}
-//             <div className="bottom flex flex-row justify-center">
-//                 {/* 좌 */}
-//                 <div className="img mr-20">
-//                     <div className="w-[500px] h-[500px] bg-gray-300 border border-gray-400 flex items-center justify-center">
-//                         이미지
-//                     </div>
-//                 </div>
-//                 {/* 우 */}
-//                 <div className="exp">
-//                     <div className="exp1 flex flex-row mb-40">
-//                         {/* 좌 */}
-//                         <div className="left mr-5">
-//                             <div className="w-[95px] h-[95px] bg-gray-300 border border-gray-400 flex items-center justify-center">
-//                                 이미지
-//                             </div>
-//                         </div>
-//                         {/* 우 */}
-//                         <div className="right flex flex-col justify-start items-start">
-//                             <h1 className="text-3xl font-extrabold">{data.explanation1}</h1>
-//                             <h1 className="text-lg font-extrabold">{data.explanation1_1}</h1>
-//                         </div>
-//                     </div>
-//                     <div className="exp2 flex flex-row mb-40">
-//                         <div className="left mr-5">
-//                             <div className="w-[95px] h-[95px] bg-gray-300 border border-gray-400 flex items-center justify-center">
-//                                 이미지
-//                             </div>
-//                         </div>
-//                         <div className="right flex flex-col justify-start items-start">
-//                             <h1 className="text-3xl font-extrabold">{data.explanation2}</h1>
-//                             <h1 className="text-lg font-extrabold">{data.explanation2_1}</h1>
-//                         </div>
-//                     </div>
-//                     <div className="exp3 flex flex-row mb-40">
-//                         <div className="left mr-5">
-//                             <div className="w-[95px] h-[95px] bg-gray-300 border border-gray-400 flex items-center justify-center">
-//                                 이미지
-//                             </div>
-//                         </div>
-//                         <div className="right flex flex-col justify-start items-start">
-//                             <h1 className="text-3xl font-extrabold">{data.explanation3}</h1>
-//                             <h1 className="text-3xl font-extrabold">{data.explanation3_0}</h1>
-//                             <h1 className="text-lg font-extrabold">{data.explanation3_1}</h1>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
--- components/solution.tsx
+++ components/solution.tsx
@@ -21,23 +21,3 @@ export default function Solution({ data }: { data: { title1: string, title2: str
         </div>
     )
 }
-
-// export default function Solution({ data }: { data: { title1: string, title2: string, explanation1: string, explanation2: string } }) {
-
-//     return (
-
-//         <div className="bg-[#2A2829] text-center w-full py-[160px] flex flex-row justify-center items-center">
-//             <div className="flex flex-col items-center">
-//                 <div className="text-left text-white flex flex-col justify-center">
-//                     <h1 className="text-[40px] font-extrabold">{data.title1}</h1>
-//                     <h1 className="text-[40px] font-extrabold">{data.title2}</h1>
-//                     <p className="mt-[30px] text-[25px]">{data.explanation1}</p>
-//                     <p className="text-[25px]">{data.explanation2}</p>
-//                 </div>
-//             </div>
-//             <div className="ml-[200px] w-[900px] h-[600px] rounded-md bg-gray-300 text-center flex items-center justify-center">
-//                 <p className="py-[320px]">image</p>
-//             </div>
-//         </div>
-//     )
-// }
