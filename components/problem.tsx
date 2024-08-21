8a7e7c5ea648ffc56b15cec65d9cfe7fa2c57ef5
jaeyeonp <wodusqkr1210@gmail.com>
2024-08-21 16:35:17 +0900

    updated

--- components/problem.tsx
+++ components/problem.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function Problem({ data }: { data: { title1: string, title2: string, paragraph1: string, paragraph2: string } }) {
     return (
-        <div className="text-center w-full h-screen flex flex-col items-center justify-center">
+        <div className="text-center w-full h-[120vh] flex flex-col items-center justify-center">
             <div className="top flex-1 w-full flex flex-col bg-white" style={{ flexBasis: '40%' }}>
                 <div className="top flex-1 flex flex-col w-full items-center justify-end">
                     <Image src={img_tmp} alt="dsf" width={60} height={60} className='rounded-xl'/>
@@ -17,8 +17,8 @@ export default function Problem({ data }: { data: { title1: string, title2: stri
                     <h1 className="text-m font-bold">{data.paragraph2}</h1>
                 </div>
             </div>
-            <div className="bot flex-1 w-full flex flex-col bg-white" style={{ flexBasis: '60%' }}>
-                <div className="img bg-[#2A2829] flex-1 flex flex-col m-10 rounded-xl">
+            <div className="bot flex-1 w-full flex flex-col items-center justify-center bg-white" style={{ flexBasis: '60%' }}>
+                <div className="img bg-[#2A2829] h-[1650px] w-[900px] flex-1 flex flex-col m-10 rounded-xl">
                     <div className="img1 flex1 flex bg-white">
                         
                     </div>
--- components/wailitst.tsx
+++ components/wailitst.tsx
@@ -1,13 +1,18 @@
+'use client'
+
+import waitlist_img from '@/img/waitlist_img.svg'
+import Image from "next/image"
+
 export default function Waitlist({ data }: { data: any }) {
     return (
-        <div className="bg-white text-center w-full h-screen flex flex-col items-center justify-center">
-            <div className="top w-full flex-1 flex flex-col items-center justify-end mb-20">
+        <div className="bg-[#2237C6] text-center w-full h-screen flex flex-col items-center justify-center">
+            <div className="top w-full flex-1 flex flex-col items-center justify-end mb-20 text-white">
                 <h1 className="text-5xl font-extrabold">{data.title1}</h1>
                 <h1 className="text-5xl font-extrabold">{data.title2}</h1>
             </div>
-            <div className="bot w-full flex-1 flex flex-col items-center justify-center">
-                <div className="w-[150px] h-[150px] bg-gray-300 border border-gray-400 flex items-center justify-center">
-                    크랭이 만나보기
+            <div className="bot w-full flex-1 flex flex-col items-center justify-start">
+                <div className="w-[150px] h-[150px] flex items-center justify-center">
+                    <Image src={waitlist_img} alt="dsf" width={150} height={150}/>
                 </div>
             </div>
         </div>
