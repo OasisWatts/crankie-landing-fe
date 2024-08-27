b0b26b0221581aba56581ca8a2617099f9b34060
jaeyeonp <wodusqkr1210@gmail.com>
2024-08-28 08:53:05 +0900

    123

--- components/front.tsx
+++ components/front.tsx
@@ -38,14 +38,6 @@ export default function Front({ data }: { data: { title1: string, title2: string
                     className="rounded-xl md:w-[200px] md:h-[200px] lg:w-[250px] lg:h-[250px]"
                 />
             </div>
-            <div
-                className="scroll-down mt-10 cursor-pointer"
-                onClick={handleScroll}
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" className="w-10 h-10 sm:w-12 sm:h-12">
-                    <path d="M12 21c-.55 0-1-.45-1-1V5.41l-5.29 5.3c-.39.38-1.02.38-1.41 0s-.39-1.03 0-1.41l7-7c.39-.39 1.02-.39 1.41 0l7 7c.39.38.39 1.03 0 1.41s-1.02.38-1.41 0L13 5.41V20c0 .55-.45 1-1 1z" />
-                </svg>
-            </div>
         </div>
     )
 }
--- components/solution.tsx
+++ components/solution.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Solution({ data }: { data: { title1: string, title2: string, explanation1: string, explanation2: string } }) {
 
     return (
-        <div className="bg-[#2881DD] w-full min-h-screen flex flex-col md:flex-row justify-end items-center p-10">
-            <div className="left flex-1 h-full flex flex-col justify-center items-center mb-8 md:mb-0 md:pr-4 ml-10" style={{ flexBasis: "50%" }}>
+        <div className="bg-[#2881DD] w-full min-h-screen flex flex-col md:flex-row justify-center items-center p-10">
+            <div className="left flex-1 flex flex-col justify-center items-center text-center mb-8 md:mb-0 md:pr-4" style={{ flexBasis: "50%" }}>
                 <div className="title mb-10">
                     <h1 className="text-2xl sm:text-3xl lg:text-4xl font-extrabold text-white">
                         정리, 이제
@@ -14,7 +14,7 @@ export default function Solution({ data }: { data: { title1: string, title2: str
                         {data.title2}.
                     </h1>
                 </div>
-                <div className="content ml-2">
+                <div className="content">
                     <h1 className="text-sm sm:text-base lg:text-lg font-bold text-[#A5CDF7]">
                         {data.explanation1}
                     </h1>
@@ -23,11 +23,11 @@ export default function Solution({ data }: { data: { title1: string, title2: str
                     </h1>
                 </div>
             </div>
-            <div className="right flex-1 h-full flex flex-col justify-center items-center mr-20" style={{ flexBasis: "50%" }}>
-                <div className="img flex justify-center items-start rounded-xl overflow-hidden ml-10">
+            <div className="right flex-1 flex justify-center items-center" style={{ flexBasis: "50%" }}>
+                <div className="img rounded-xl overflow-hidden">
                     <Image unoptimized src={img_tmp} alt="Solution Image" width={622} height={473} className="w-full h-auto" />
                 </div>
             </div>
         </div>
-    )
+    );
 }
